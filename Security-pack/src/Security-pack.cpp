b2406676e35f8f7b06dbd4a1cac6d344e05e99db
Nikodem Gapski <59776891+NikodemGapski@users.noreply.github.com>
2020-09-01 14:24:03 +0200

    Clear the main file Security-pack.cpp
    Removed trash from the main() function.

--- Security-pack/src/Security-pack.cpp
+++ Security-pack/src/Security-pack.cpp
@@ -7,20 +7,5 @@
 int main()
 {
 	srand(time(NULL));
-	/// conclusion: we have a problem... custom INT is way too SLOOOOOOOOOOOOOOOOOOW
-
-	//std::cout << a.divide(INT(4));
-	//MainController::Run();
-	// example code for RSA
-	/*std::string message = "abc\nabc";
-	_int64 n = 161322719, privateKey = 159827143, publicKey = 14839;
-	//Math::InitRSA(n, publicKey, privateKey);
-	message = Math::StringToData(message);
-	std::cout << message << std::endl;
-	message = Math::PerformRSA(message, publicKey, n);
-	std::cout << message << std::endl;
-	message = Math::PerformRSA(message, privateKey, n);
-	std::cout << message << std::endl;
-	message = Math::DataToString(message);
-	std::cout << std::endl << message;*/
+	MainController::Run();
 }
